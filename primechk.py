a5826970d631516c82515d755b85c84386e8d388
Chuck Bennett <cnbennett3@gmail.com>
2020-08-25 17:06:31 -0400

    Remove more lint errors.

--- primechk.py
+++ primechk.py
@@ -1,11 +1,11 @@
 num = 39
 if num > 1:
     for i in range(2, num):
-    if (num % i) == 0:
-        print(num, " is not a prime.")
-        print(i, " times ", num//i, " is ", num, ".")
-        break
-    else:
-        print(num, " is a prime.")
+        if (num % i) == 0:
+            print(num, " is not a prime.")
+            print(i, " times ", num//i, " is ", num, ".")
+            break
+        else:
+            print(num, " is a prime.")
 else:
     print(num, " is not a prime.")
