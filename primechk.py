f0dea593c73d3d79ddfb566205f4a74905914cc6
cnbennett3 <53792237+cnbennett3@users.noreply.github.com>
2020-08-25 16:39:51 -0400

    Update primechk.py

--- primechk.py
+++ primechk.py
@@ -1,11 +1,11 @@
-num = 407
+num = 39
 if num > 1:
   for i in range(2, num):
     if (num % i) == 0:
       print(num, " is not a prime.")
       print(i," times ", num//i, " is ", num, ".")
-      break
+       break
     else:
-            print(num, " is a prime.")
+      print(num, " is a prime.")
 else:
   print(num, " is not a prime.")
